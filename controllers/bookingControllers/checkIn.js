83485f72fb2c51f9ffcefbc5057ca0a023e2d6e4
kolanpaka kalyan <kolanpaka2000@gmail.com>
2025-11-20 19:23:30 +0530

    updated confirmation check for checkin/checkout

--- controllers/bookingControllers/checkIn.js
+++ controllers/bookingControllers/checkIn.js
@@ -2,6 +2,7 @@ const Booking = require("./../../models/bookingModel");
 const NodeError = require("./../../utils/nodeError");
 
 async function checkIn(req, res) {
+  const { confirmation, breakFast } = req.body;
   const booking = await Booking.findById(req.params.id).populate([
     "userId",
     "cabinId",
@@ -9,9 +10,16 @@ async function checkIn(req, res) {
   if (!booking) {
     throw new NodeError(true, "booking not found!!", 404);
   }
-
+  if (!confirmation) {
+    throw new NodeError(
+      true,
+      "Kindly confirm if you would like to proceed with the check-in.",
+      400
+    );
+  }
   if (booking.status === "unconfirmed") {
     booking.paid = true;
+    booking.breakFast = breakFast;
     booking.status = "checked in";
     await booking.save();
   }
--- controllers/bookingControllers/checkOut.js
+++ controllers/bookingControllers/checkOut.js
@@ -2,12 +2,20 @@ const Booking = require("./../../models/bookingModel");
 const NodeError = require("./../../utils/nodeError");
 
 async function checkOut(req, res) {
+  const { confirmation } = req.body;
   const booking = await Booking.findById(req.params.id).populate([
     "userId",
     "cabinId",
   ]);
   if (!booking) {
     throw new NodeError(true, "booking not found!!", 404);
+  }
+  if (!confirmation) {
+    throw new NodeError(
+      true,
+      "Kindly confirm if you would like to proceed with the check-in.",
+      400
+    );
   }
 
   if (booking.status === "checked in") {
